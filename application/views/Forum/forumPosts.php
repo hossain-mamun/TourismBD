4284b7f18c57d811e554567d452527ecb9620ec6
hossain-mamun <hossainmamun035@gmail.com>
2015-11-27 23:24:15 +0600

    forum post updated

--- application/views/Forum/forumPosts.php
+++ application/views/Forum/forumPosts.php
@@ -1,24 +1,22 @@
      <?php 
-   for ($i=0; $i <5 ; $i++) { 
-   	
-   
-  ?>
+
+   foreach ($forum_details as $forum_post_details) {
+   ?>
    <div class='container forumBG'>
     <div class="row">
  	 <div class="forumPosts col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-1 ">
- 	   <h3><strong>Cox's Bazar Accomadation Facility</strong></h3>
- 	    Written By:<strong> Hossain Mamun</strong><br/>
- 	    Written Time: <strong>25-11-2015</strong>
- 	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. ... 
- 	    up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
+ 	   <h3><strong> <a href="#"> <?php echo $forum_post_details->topic_title?></a></strong></h3>
+ 	    Written By:<strong><?php echo $forum_post_details->user_name?></strong><br/>
+ 	    Written Time: <strong><?php echo $forum_post_details->written_time?></strong>
+ 	    <p><?php echo substr($forum_post_details->description,0,110) ;?></p>
  	 </div>
  	   <div class="col-xs-10 col-xs-offset-1 col-sm-2 col-sm-offset-0 forumComment">
  		 <div class="row">
  		  <div class="col-sm-12 forumCommentSub">
- 			<i class="fa fa-comments fa-2x"> 10</i>
+ 			<i class="fa fa-comments fa-1x"> 10</i>
  		  </div>
  		  <div class="col-sm-12 forumCommentSub">
- 		  	<i class="fa fa-eye fa-2x"> 30</i>
+ 		  	<i class="fa fa-eye fa-1x"> 30</i>
  		  </div>
  		 </div>
  	  </div>
--- application/views/Forum/startNewTopic.php
+++ application/views/Forum/startNewTopic.php
@@ -1,19 +1,32 @@
   <div class='container forumBG'>
     <div class="row ForumTopicBG">
-     <form action="#" method="post">
+      <?php
+       if (validation_errors()) {?>
+       <div class="loginAndSignupAlert"> 
+      <?php echo validation_errors();?>
+      </div>
+      <?php }
+       elseif (isset($error)) {?>
+           <div class="loginAndSignupAlert">
+           <?php echo $error;?>
+           </div>
+      <?php     
+       }
+      ?>
+     <form action="<?php echo site_url('tourism_con/forum_post_validation');?>" method="post">
  	  <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 ForumInputDesign">
  	    <div class="form-group ">
-          <input type="text" class="form-control" name="name" id="Name" placeholder="Enter Topic Title"/>
+          <input type="text" class="form-control" name="topic_title" id="Topic" placeholder="Enter Topic Title" required/>
          </div> 
       </div>
  	 <div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-2 forumPlaceDropdown ">
       <div class="form-group">
-        <select class="form-control" id="catg" name="categories">
+        <select class="form-control" id="catg" name="place_select">
           <option value="">Tourist Place</option>
           <?php
              foreach ($place_name as $place_selection) {
            ?>
-          <option value="<?php echo $place_selection->place_title;?>"><?php echo $place_selection->place_title;?></option>
+          <option value="<?php echo $place_selection->place_title;?>"><?php echo set_select('place_select', '<?php echo $place_selection->place_title;?>'); ?><?php echo $place_selection->place_title;?></option>
           <?php }?>
           <option value="">Others</option>
         </select>
@@ -21,38 +34,43 @@
     </div>
      <div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 forumPlaceDropdown ">
       <div class="form-group">
-        <select class="form-control" id="catg1" name="subCategories">
+        <select class="form-control" id="catg1" name="sub_place_select">
           <option value="">Related Place</option>
            <?php
              foreach ($place_name as $sub_place_selection) {
            ?>
            <?php if ($sub_place_selection->related_place_title1!=null) {?>
            <option value="<?php echo $sub_place_selection->related_place_title1;?>">
+           <?php echo set_select('sub_place_select', '<?php echo $sub_place_selection->related_place_title1;?>'); ?>
           <?php echo $sub_place_selection->related_place_title1;?>
           </option>
           <?php  }?>
           <?php if ($sub_place_selection->related_place_title2!=null) {?>
           <option value="<?php echo $sub_place_selection->related_place_title2;?>">
+          <?php echo set_select('sub_place_select', '<?php echo $sub_place_selection->related_place_title2;?>'); ?>
           <?php echo $sub_place_selection->related_place_title2;?>
           </option>
           <?php  }?>
           <?php if ($sub_place_selection->related_place_title3!=null) {?>
            <option value="<?php echo $sub_place_selection->related_place_title3;?>">
+           <?php echo set_select('sub_place_select', '<?php echo $sub_place_selection->related_place_title3;?>'); ?>
           <?php echo $sub_place_selection->related_place_title3;?>
           </option>
           <?php  }?>
           <?php if ($sub_place_selection->related_place_title4!=null) {?>
            <option value="<?php echo $sub_place_selection->related_place_title4;?>">
+           <?php echo set_select('sub_place_select', '<?php echo $sub_place_selection->related_place_title4;?>'); ?>
           <?php echo $sub_place_selection->related_place_title4;?>
           </option>
           <?php  }?>
           <?php }?>
-          <option value="">Others</option>
+          <option value="others"><?php echo set_select('sub_place_select', 'others'); ?>Others</option>
         </select>
       </div>
     </div>
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 ForumDescriptDesign">
- 	   <textarea class="form-control" rows="7" name='post' id="#" placeholder="Write Your Post"></textarea> 
+ 	   <textarea class="form-control" rows="7" name='post' id="#" placeholder="Write Your Post" required></textarea> 
+ 	   <button type="reset" class="btn btn-warning">Reset</button>
  	   <button type="submit" class="btn btn-primary">Post</button>
       </div>
  	 </form>	 
