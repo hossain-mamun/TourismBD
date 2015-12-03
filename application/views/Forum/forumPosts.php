6c80ee6a5e240a0b41be05b25b988ae637de81f1
Hossain Mamun <hossainmamun035@gmail.com>
2015-12-03 21:03:01 +0600

    Update forumPosts.php

--- application/views/Forum/forumPosts.php
+++ application/views/Forum/forumPosts.php
@@ -1,9 +1,8 @@
 	      
-      	 <div class="row forumwell">
+      	<div class="row forumwell">
           <div class="col-sm-12 ">
            <div class="well well-lg">
              <h2>Post Gallery</h2>
-              
            </div>
           </div>
          </div>
@@ -20,25 +19,22 @@
 	 	    Written By:<strong><?php echo $forum_post_details->user_name?></strong><br/>
 	 	    Posted on <strong><?php echo $forum_post_details->written_time?></strong>
 	 	    <p><?php echo substr($forum_post_details->description,0,105) ;?> ...</p>
-	 	 </div>
-	 	   <div class="col-xs-10 col-xs-offset-1 col-sm-2 col-sm-offset-0 forumComment">
-	 		 <div class="row">
-	 		  <div class="col-sm-12 forumCommentSub">
-	 			<i class="fa fa-comments fa-1x"> 10</i>
-	 		  </div>
-	 		  <div class="col-sm-12 forumCommentSub">
-	 		  	<i class="fa fa-eye fa-1x"> <?php echo $forum_post_details->post_views?></i>
-	 		  </div>
-	 		 </div>
 	 	  </div>
-	 	</div>
+	 	   <di v class="col-xs-10 col-xs-offset-1 col-sm-2 col-sm-offset-0 forumComment">
+	             <div class="row">
+	 	       <div class="col-sm-12 forumCommentSub">
+	 	        <i class="fa fa-comments fa-1x"> 10</i>
+	 	       </div>
+	 	      <div class="col-sm-12 forumCommentSub">
+	 	       <i class="fa fa-eye fa-1x"> <?php echo $forum_post_details->post_views?></i>
+	 	      </div>
+	 	     </div>
+	 	    </div>
+	    </div>
 	 
 	 <?php }?>
-
-  
-	<div class="col-sm-4 col-sm-offset-4">
-		<?php echo $this->pagination->create_links();?>
-	</div>
-  
-</div> 
+          <div class="col-sm-4 col-sm-offset-4">
+           <?php echo $this->pagination->create_links();?>
+	  </div>
+ </div> 
 
