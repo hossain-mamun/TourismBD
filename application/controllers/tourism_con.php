962a0747491183490bc279d31f69b446ff474f62
Hossain Mamun <hossainmamun035@gmail.com>
2015-12-03 20:56:51 +0600

    Update tourism_con.php

--- application/controllers/tourism_con.php
+++ application/controllers/tourism_con.php
@@ -5,60 +5,60 @@ class Tourism_con extends CI_Controller
 {
 	
 	public function index(){
-		    $config['base_url'] = site_url('tourism_con/index');  
-			$config['total_rows'] = $this->db->get('tourism_contents')->num_rows();
-			$config['uri_segment'] = 3;
-			$config['per_page'] = 4; 
-			$config['full_tag_open'] = "<ul class='pagination pagination-lg'>";
-			$config['full_tag_close'] ="</ul>";
-			$config['num_tag_open'] = "<li>";
-			$config['num_tag_close'] = '</li>';
-			$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
-			$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
-			$config['next_tag_open'] = "<li>";
-			$config['next_tagl_close'] = "</li>";
-			$config['prev_tag_open'] = "<li>";
-			$config['prev_tagl_close'] = "</li>";
-			$config['first_tag_open'] = "<li>";
-			$config['first_tagl_close'] = "</li>";
-			$config['last_tag_open'] = "<li>";
-			$config['last_tagl_close'] = "</li>";
-			$data['place_contents']=$this->tourism_model->homepage($config);
-			$data['homepage']='Home';
-			$data['home1']='home1';
-			$data['carousel']='carousel';
-			$this->load->view('home/homepage',$data);
+	        $config['base_url'] = site_url('tourism_con/index');  
+	        $config['total_rows'] = $this->db->get('tourism_contents')->num_rows();
+	        $config['uri_segment'] = 3;
+		$config['per_page'] = 4; 
+		$config['full_tag_open'] = "<ul class='pagination pagination-lg'>";
+		$config['full_tag_close'] ="</ul>";
+		$config['num_tag_open'] = "<li>";
+		$config['num_tag_close'] = '</li>';
+		$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+		$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+		$config['next_tag_open'] = "<li>";
+		$config['next_tagl_close'] = "</li>";
+		$config['prev_tag_open'] = "<li>";
+		$config['prev_tagl_close'] = "</li>";
+		$config['first_tag_open'] = "<li>";
+		$config['first_tagl_close'] = "</li>";
+		$config['last_tag_open'] = "<li>";
+		$config['last_tagl_close'] = "</li>";
+		$data['place_contents']=$this->tourism_model->homepage($config);
+		$data['homepage']='Home';
+		$data['home1']='home1';
+		$data['carousel']='carousel';
+		$this->load->view('home/homepage',$data);
 	}
 			
 
-	public function sitemap(){
-     $data['title']='Sitemap';
-     $data['sitemap']='sitemap';
-     $this->load->view('home/homepage',$data);
+        public function sitemap(){
+	       $data['title']='Sitemap';
+	       $data['sitemap']='sitemap';
+	       $this->load->view('home/homepage',$data);
 	}
 
-	public function full_place(){
+        public function full_place(){
 		$place_id = $_GET['place_id'];
 		$data['result']=$this->tourism_model->get_full_place($place_id);
 		$this->tourism_model->place_views($place_id);
-        $this->load->view('full_place',$data);
+	        $this->load->view('full_place',$data);
 	}
-	public function contact(){
+    public function contact(){
 		$data['contact']='Contact Us';
 		$data['contact1']='contact1';
 		$this->load->view('home/homepage',$data);
 	}
 	public function contact_validation(){
 
-         $this->form_validation->set_rules('name', 'Name', 'required');
-         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-         $this->form_validation->set_rules('message', 'Message', 'required');
-         if ($this->form_validation->run() == FALSE)
-		{ 
-		$data['contact']='Contact Us';
-		$data['contact1']='contact1';
-		$this->load->view('home/homepage',$data);
-	    }
+	         $this->form_validation->set_rules('name', 'Name', 'required');
+	         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+	         $this->form_validation->set_rules('message', 'Message', 'required');
+	         if ($this->form_validation->run() == FALSE)
+	         { 
+		 $data['contact']='Contact Us';
+		 $data['contact1']='contact1';
+		 $this->load->view('home/homepage',$data);
+	         }
 	    else{
 	    	$result=$this->tourism_model->mail_send();
    
