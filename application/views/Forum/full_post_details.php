cc8dd4f47fc2a751de216b05ff187ed4e0f766fe
Hossain Mamun <hossainmamun035@gmail.com>
2015-12-03 21:06:40 +0600

    Update full_post_details.php

--- application/views/Forum/full_post_details.php
+++ application/views/Forum/full_post_details.php
@@ -7,10 +7,10 @@
    <h2><strong ><?php echo $post_results->topic_title?></strong></h2>
     Written By:<strong><?php echo $post_results->user_name?></strong><br/>
     <hr/>
-	 <p><?php echo $post_results->description ;?></p>
-	 <div class="row forum_full_Posts_bottom">
-	 	<div class=" col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-1">
-	 	  <a href="#"><i class="fa fa-thumbs-o-up fa-lg">&nbsp;&nbsp;6</i></a><a href="#"><i class="fa fa-thumbs-o-down fa-lg">&nbsp;&nbsp;3</i></a><i class="fa fa-clock-o fa-lg"></i>&nbsp;&nbsp;<small>Posted on: <?php echo $post_results->written_time?></small> 
+     <p><?php echo $post_results->description ;?></p>
+      <div class="row forum_full_Posts_bottom">
+       <div class=" col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-1">
+	 <a href="#"><i class="fa fa-thumbs-o-up fa-lg">&nbsp;&nbsp;6</i></a><a href="#"><i class="fa fa-thumbs-o-down fa-lg">&nbsp;&nbsp;3</i></a><i class="fa fa-clock-o fa-lg"></i>&nbsp;&nbsp;<small>Posted on: <?php echo $post_results->written_time?></small> 
        </div>
       </div> 
        </div>
@@ -22,7 +22,7 @@
          ?>
        <div class="row">
         <div class=" col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-1 commentPublished">
-        	<strong><?php echo $comments_results->comment_post_by?></strong>:&nbsp;<small><?php echo $comments_results->comment?></small>
+         <strong><?php echo $comments_results->comment_post_by?></strong>:&nbsp;<small><?php echo $comments_results->comment?></small>
         </div>
        </div>
        <?php }
